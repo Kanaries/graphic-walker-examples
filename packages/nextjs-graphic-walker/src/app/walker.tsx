12a837005593cad9e7d9a4a407be103b916a8271
observedobserver <270001151@qq.com>
2023-09-29 11:14:57 -0700

    fix: nextjs gw bug

--- packages/nextjs-graphic-walker/src/app/walker.tsx
+++ packages/nextjs-graphic-walker/src/app/walker.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import { GraphicWalker } from '@kanaries/graphic-walker'
+import dynamic from 'next/dynamic';
+
+const GraphicWalker = dynamic(() => {
+    return import('@kanaries/graphic-walker').then(res => res.GraphicWalker)
+}, {
+    ssr: false
+})
 
 function Walker () {
     return <GraphicWalker />
